bc5b856593558bc7ef54899bb3c9b4ce040d7eb3
arun <arunr181297@gmail.com>
2021-07-17 07:53:20 +0530

    changes by arun

--- src/main/java/in/lmscore/util/StringUtil.java
+++ src/main/java/in/lmscore/util/StringUtil.java
@@ -14,15 +14,15 @@ public class StringUtil {
 	public static void main(String[] args) {
 		String userName = "Reka";
 		String passWord = "123678";
-		Logger.debug("userName:" + StringUtil.isValidString(userName));
-		Logger.debug("passWord:" + StringUtil.isValidString(passWord));
+		System.out.println("userName:" + StringUtil.isValidString(userName));
+		System.out.println("passWord:" + StringUtil.isValidString(passWord));
 
 		// Task 1: If firstName and lastName is valid, then we will store user details
 		// in "users" table.
 		if (StringUtil.isValidString(userName) && StringUtil.isValidString(passWord)) {
-			Logger.debug("Success");
+			System.out.println("Success");
 		} else {
-			Logger.debug("Data is invalid");
+			System.out.println("Data is invalid");
 		}
 	}
 
