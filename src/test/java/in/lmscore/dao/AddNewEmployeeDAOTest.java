c2c1ac62f0e5b3ad747128db2866b0ef2f592924
arun <arunr181297@gmail.com>
2021-07-21 12:09:37 +0530

    committed by arun for add new employee dao test

---
+++ src/test/java/in/lmscore/dao/AddNewEmployeeDAOTest.java
@@ -0,0 +1,12 @@
+package in.lmscore.dao;
+
+import java.sql.SQLException;
+
+public class AddNewEmployeeDAOTest {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		AddNewEmployeeDAO.addNewEmployee();
+
+	}
+
+}
